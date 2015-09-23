30859b5c5133c28540fd48cd5499849f8f6b140f
Prince Kumar <prince18pat@gmail.com>
2015-09-24 00:13:16 +0900

    Table view custom table

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Prince Kumar on 9/24/15.
+//  Copyright © 2015 Prince Kumar. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
