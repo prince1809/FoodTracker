bff546307f8d83ca70cf903cf5f056abb161e97d
Prince Kumar <prince18pat@gmail.com>
2015-09-23 15:51:47 +0900

    Rating control button

---
+++ RatingControl.swift
@@ -0,0 +1,38 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Prince Kumar on 9/23/15.
+//  Copyright © 2015 Prince Kumar. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+    
+    //MARK: Initialization
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        
+        
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        button.backgroundColor = UIColor.redColor()
+        button.addTarget(self, action: "ratingButtonTapped:", forControlEvents: .TouchDown)
+        addSubview(button)
+        
+    }
+    
+    override func intrinsicContentSize() -> CGSize {
+        return CGSize(width: 240, height: 44)
+    }
+    
+    
+    //MARK: Button Action
+    
+    func ratingButtonTapped(button: UIButton){
+        print("Button Pressed ");
+    }
+
+}
